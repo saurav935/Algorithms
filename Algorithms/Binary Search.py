101a5dd5f6fc0bd27eb66a2cda894c2f8e4f7a67
saurav <75733364+saurav935@users.noreply.github.com>
2021-08-31 12:40:33 +0530

    Add files via upload

---
+++ Algorithms/Binary Search.py
@@ -0,0 +1,26 @@
+
+
+def binary_search(arr,target):
+    l = 0
+    r = len(arr) - 1
+
+    while l <= r:
+        # find the middle element
+        # we use (l+(r-l)) instead of (l+r) because, if l and r are very large numbers, it might be a case that the addition of both of them results into a value that is beyond the range of python's limit to store.
+        mid = l + (r-l) // 2
+
+        if arr[mid] < target:
+            l = mid+1
+
+        elif arr[mid] > target:
+            r = mid-1
+
+        # target found
+        else:
+            return mid
+
+    return -1
+
+arr = [-18,-12,-4,0,2,3,4,15,16,18,22,45,89]
+target = 4566
+print(binary_search(arr,target))
