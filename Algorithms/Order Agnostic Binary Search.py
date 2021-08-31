11a91aebd74cda181876332f37ea388e019caacd
saurav <75733364+saurav935@users.noreply.github.com>
2021-08-31 13:13:58 +0530

    Add files via upload

---
+++ Algorithms/Order Agnostic Binary Search.py
@@ -0,0 +1,32 @@
+
+
+def order_agnostic_binary_search(arr,target):
+    l = 0
+    r = len(arr) - 1
+    is_ascending = arr[l] < arr[r]
+
+    while l <= r:
+        mid = l + (r-l) // 2
+
+        if arr[mid] == target:
+            return mid
+
+        if is_ascending:
+            if arr[mid] < target:
+                l = mid+1
+
+            else:
+                r = mid-1
+
+        else:
+            if arr[mid] > target:
+                l = mid+1
+            else:
+                r = mid-1
+
+    return -1
+
+arr = [89, 45, 22, 18, 16, 15, 4, 3, 2, 0, -4, -12, -18]
+target = 89
+print(order_agnostic_binary_search(arr,target))
+
