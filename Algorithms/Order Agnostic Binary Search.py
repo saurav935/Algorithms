873318b76bb1af58f2484a36ba48fc78b5f86f8b
saurav <75733364+saurav935@users.noreply.github.com>
2021-08-31 16:13:42 +0530

    Update Order Agnostic Binary Search.py

--- Algorithms/Order Agnostic Binary Search.py
+++ Algorithms/Order Agnostic Binary Search.py
@@ -1,27 +1,27 @@
 
 
 def order_agnostic_binary_search(arr,target):
-    l = 0
-    r = len(arr) - 1
-    is_ascending = arr[l] < arr[r]
+    start = 0
+    end = len(arr) - 1
+    is_ascending = arr[start] < arr[end]
 
-    while l <= r:
-        mid = l + (r-l) // 2
+    while start <= end:
+        mid = start + (end - start) // 2
 
         if arr[mid] == target:
             return mid
 
         if is_ascending:
             if arr[mid] > target:
-                r = mid-1
+                end = mid-1
             else:
-                l = mid+1
+                start = mid+1
 
         else:
             if arr[mid] < target:
-                r = mid-1
+                end = mid-1
             else:
-                l = mid+1
+                start = mid+1
 
     return -1
 
