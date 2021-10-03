8c779768f61faf21fb13b79a6e0730ff355bbee7
saurav <75733364+saurav935@users.noreply.github.com>
2021-10-03 10:44:38 +0530

    Add files via upload

---
+++ Sorting algorithms/Bubble Sort.py
@@ -0,0 +1,35 @@
+
+'''
+
+1. Time cpmplexity:
+Best case: O(n)
+Worst case: O(n^2)
+
+2. Space complexity: O(1)
+
+3. It is a STABLE sorting algorithm
+'''
+
+
+
+def bubble_sort(arr):
+    n = len(arr)
+
+    if n == 1:
+        return arr
+
+    for i in range(0,n):
+        swapped = False
+        for j in range(1,n-i):
+            if arr[j-1] > arr[j]:
+                arr[j],arr[j-1] = arr[j-1],arr[j]
+                swapped = True
+
+        if not swapped:
+            break
+
+    return arr
+
+
+arr = [3,1,5,4,2]
+print(bubble_sort(arr))
